43b8fce75943b1c6345be829548110a9ee2e9db4
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-03-26 19:09:12 -0300

    feat: implemented error fail to create

---
+++ src/core/shared/errors/FailToCreateError.ts
@@ -0,0 +1,11 @@
+import { DomainError } from "@core/domain/errors/DomainError"
+
+export class FailToCreateError extends DomainError {
+  constructor(context: string){
+    super({
+      message: `Fail to Create a ${context}`,
+      context,
+      name: 'FailToCreateError'
+    })
+  }
+}
