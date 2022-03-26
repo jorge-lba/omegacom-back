ca18945128f771292a62d8da2fee4d1f38c6fcb7
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-03-26 19:11:30 -0300

    feat: implemented provider to uuid

---
+++ src/core/shared/Uuid.ts
@@ -0,0 +1,16 @@
+export class Uuid {
+  static generate() {
+    const RANGE = 20
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < RANGE; i++) {
+        result += characters
+          .charAt(
+            Math.floor(Math.random() * characters.length)
+          );
+    }
+
+    return result;
+  }
+}
