dcd514a22aeea5ba0caad3061f42f5ab45e34e12
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-03-26 19:21:54 -0300

    feat: implemented stock repository

---
+++ src/modules/stock/repositories/StockRepository.ts
@@ -0,0 +1,9 @@
+import { Stock } from "../domain/Stock";
+
+export interface StockRepository {
+  exists(props: ExistsProps): Promise<boolean>;
+  findById(id: string): Promise<Stock>;
+  save(stock: Stock): Promise<Stock>;
+}
+
+export type ExistsProps = Stock | { name: string, branch: string }
