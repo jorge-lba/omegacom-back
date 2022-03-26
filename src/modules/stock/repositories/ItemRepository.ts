0e21135189aa7a94b48c4d2eeb1bc317e0c8bab8
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-03-26 19:22:05 -0300

    feat: implemented item repository

---
+++ src/modules/stock/repositories/ItemRepository.ts
@@ -0,0 +1,6 @@
+import { Item } from "../domain/Item";
+
+export interface ItemRepository {
+  exists(id: string): Promise<boolean>;
+  bulkSave(items: Item[]): Promise<Item[]>;
+}
---
+++ src/modules/stock/repositories/inMemory/ItemRepositoryInMemory.ts
@@ -0,0 +1,15 @@
+import { Item } from "@modules/stock/domain/Item";
+import { ItemRepository } from "../ItemRepository";
+
+export class ItemRepositoryInMemory implements ItemRepository {
+  private _items: Map<string, Item> = new Map()
+
+  exists(id: string): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+
+  async bulkSave(items: Item[]): Promise<Item[]> {
+    items.forEach(item => this._items.set(item.id, item))
+    return items
+  }
+}
