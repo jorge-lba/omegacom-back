c07cc1e61d72087232a38f68b8fc0df602acb656
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2022-03-26 19:10:23 -0300

    feat: implemented error invalid entity

---
+++ src/core/shared/errors/InvalidEntityError.ts
@@ -0,0 +1,11 @@
+import { DomainError } from "@core/domain/errors/DomainError"
+
+export class InvalidEntityError extends DomainError {
+  constructor(context: string){
+    super({
+      message: `${context} is invalid`,
+      context,
+      name: 'InvalidEntityError'
+    })
+  }
+}
